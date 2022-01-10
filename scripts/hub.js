102d88206a05dae6429b866ee1cb46df7c4b5eb1
Nigel Powell <nigel@thesadsongco.com>
2022-01-10 15:49:12 +0000

    Save to and Load from disk

--- scripts/hub.js
+++ scripts/hub.js
@@ -1,9 +1,103 @@
 import { getSetlist, sortSetlistByOrder } from './modules/common/getSetlist.js';
 
-let setlist;
+let setlist, setlistContainer;
 
 window.onload = () => {
-    slist('setlist');
+    setlistContainer = document.getElementById('setlist');
+    slist(setlistContainer);
+}
+
+const createSetlistUI = (setlist, target) => {
+    while (target.firstChild)
+        target.removeChild(target.firstChild);
+    target.classList.add('slist');
+    let items = [];
+    let current = null;
+    if (setlist.length === 0) {
+        const emptyMessage = document.createElement('div');
+        emptyMessage.classList.add('songNameContainer', 'emptyMessage');
+        emptyMessage.innerText = "Setlist items will appear here after you save them using 'Create New Song Visual'";
+        target.appendChild(emptyMessage);
+        return
+    }
+    const sortedSetlist = sortSetlistByOrder(setlist);
+    for (const setlistItem of sortedSetlist) {
+        const item = document.createElement('li');
+        item.value = parseInt(setlistItem.position);
+        const songNameContainer = document.createElement('div')
+        songNameContainer.classList.add('songNameContainer');
+        songNameContainer.innerText = setlistItem.name;
+        item.appendChild(songNameContainer);
+        item.draggable = true;
+    
+        // add edit and delete buttons to list
+        const options = document.createElement('div');
+        options.classList.add('setlistItemOptions');
+        const delItem = document.createElement('a');
+        const editItem = document.createElement('a');
+        editItem.innerText = 'edit';
+        editItem.classList.add('optButton');
+        editItem.id = setlistItem.name;
+        editItem.addEventListener('click', editSetlistItem);
+        options.appendChild(editItem);
+        delItem.innerText = 'delete';
+        delItem.classList.add('optButton');
+        delItem.id = setlistItem.name;
+        delItem.addEventListener('click', deleteSetlistItem);
+        options.appendChild(delItem);
+        item.appendChild(options);
+    
+        item.addEventListener('dragstart', (e) => {
+            current = e.target;
+            current.classList.add('grabbed');
+            e.target.querySelector('.setlistItemOptions').style.display = 'none';
+            for (let targetItem of items) {
+                if (targetItem != current) targetItem.classList.add('hint');
+            }
+        });
+    
+        item.addEventListener('dragenter', (e) => {
+            if (e.target != current) e.target.classList.add('active');
+        })
+    
+        item.addEventListener('dragleave', (e) => {
+            e.target.classList.remove('active');
+        });
+    
+        item.addEventListener('dragend', () => {
+            for (let targetItem of items) {
+                targetItem.classList.remove('hint');
+                targetItem.classList.remove('active');
+            }
+        });
+    
+        item.addEventListener('dragover', (e) => {
+            e.preventDefault();
+        });
+    
+        item.addEventListener('drop', (e) => {
+            // TODO only works on first drag and drop?
+            e.preventDefault();
+            current.classList.remove('grabbed');
+            current.querySelector('.setlistItemOptions').style.display = 'block';
+            if (e.target != current) {
+                let currentpos = 0, droppedpos = 0;
+                for (let i = 0; i < items.length; i ++) {
+                    if (current == items[i]) currentpos = i;
+                    if (e.target == items[i]) droppedpos = i;
+                }
+                // TODO there's an error here, but non-fatal
+                if (currentpos < droppedpos)
+                    e.target.parentNode.insertBefore(current, e.target.nextSibling);
+                else
+                    e.target.parentNode.insertBefore(current, e.target);
+                saveSetlist();
+            }
+        });
+        target.appendChild(item);
+        items.push(item);
+    }
+
 }
 
 /**
@@ -11,105 +105,22 @@ window.onload = () => {
  * @param {HTMLElement} target - container for sortable list
  */
 const slist = (target) => {
-    target = document.getElementById(target);
-    while (target.firstChild)
-        target.removeChild(target.firstChild);
-    target.classList.add('slist');
-    let items = [];
-    let current = null;
+    // target = document.getElementById(target);
     getSetlist()
     .then((res) => {
         setlist = res;
-        if (res.length === 0) {
-            const emptyMessage = document.createElement('div');
-            emptyMessage.classList.add('songNameContainer', 'emptyMessage');
-            emptyMessage.innerText = "Setlist items will appear here after you save them using 'Create New Song Visual'";
-            target.appendChild(emptyMessage);
-            return
-        }
-        const sortedSetlist = sortSetlistByOrder(setlist);
-        for (const setlistItem of sortedSetlist) {
-            const item = document.createElement('li');
-            item.value = parseInt(setlistItem.position);
-            const songNameContainer = document.createElement('div')
-            songNameContainer.classList.add('songNameContainer');
-            songNameContainer.innerText = setlistItem.name;
-            item.appendChild(songNameContainer);
-            item.draggable = true;
-
-            // add edit and delete buttons to list
-            const options = document.createElement('div');
-            options.classList.add('setlistItemOptions');
-            const delItem = document.createElement('a');
-            const editItem = document.createElement('a');
-            editItem.innerText = 'edit';
-            editItem.classList.add('optButton');
-            editItem.id = setlistItem.name;
-            editItem.addEventListener('click', editSetlistItem);
-            options.appendChild(editItem);
-            delItem.innerText = 'delete';
-            delItem.classList.add('optButton');
-            delItem.id = setlistItem.name;
-            delItem.addEventListener('click', deleteSetlistItem);
-            options.appendChild(delItem);
-            item.appendChild(options);
-        
-            item.addEventListener('dragstart', (e) => {
-                current = e.target;
-                current.classList.add('grabbed');
-                e.target.querySelector('.setlistItemOptions').style.display = 'none';
-                for (let targetItem of items) {
-                    if (targetItem != current) targetItem.classList.add('hint');
-                }
-            });
-        
-            item.addEventListener('dragenter', (e) => {
-                if (e.target != current) e.target.classList.add('active');
-            })
-        
-            item.addEventListener('dragleave', (e) => {
-                e.target.classList.remove('active');
-            });
-        
-            item.addEventListener('dragend', () => {
-                for (let targetItem of items) {
-                    targetItem.classList.remove('hint');
-                    targetItem.classList.remove('active');
-                }
-            });
-        
-            item.addEventListener('dragover', (e) => {
-                e.preventDefault();
-            });
-        
-            item.addEventListener('drop', (e) => {
-                e.preventDefault();
-                current.classList.remove('grabbed');
-                current.querySelector('.setlistItemOptions').style.display = 'block';
-                if (e.target != current) {
-                    let currentpos = 0, droppedpos = 0;
-                    for (let i = 0; i < items.length; i ++) {
-                        if (current == items[i]) currentpos = i;
-                        if (e.target == items[i]) droppedpos = i;
-                    }
-                    // TODO there's an error here, but non-fatal
-                    if (currentpos < droppedpos)
-                        e.target.parentNode.insertBefore(current, e.target.nextSibling);
-                    else
-                        e.target.parentNode.insertBefore(current, e.target);
-                    saveSetlist();
-                }
-            });
-            target.appendChild(item);
-            items.push(item);
-        }
+        createSetlistUI(setlist, target);
+        setDownloadButton(setlist);
     })
+    .catch((err) => {
+        throw new Error(err);
+    });
 }
 
 /**
  * Save sorted setlist to database
  */
-const saveSetlist = () => {
+const saveSetlist = (replace=false) => {
     const setlistItems = document.getElementById('setlist').getElementsByTagName('li');
     for (const [setlistPos, setlistItem] of Object.entries(setlistItems)) {
         console.log(setlistItem.firstChild.innerText)
@@ -120,17 +131,26 @@ const saveSetlist = () => {
     }
     let openRequest = indexedDB.open('visDB', 1);
     openRequest.onerror = (err) => {
-        console.log(`Error opening database: ${err.message}`);
+        throw new Error(`Error opening database: ${err.message}`);
     }
     openRequest.onsuccess = () => {
         let db = openRequest.result;
         const transaction = db.transaction('setlist', 'readwrite');
         const setListFromDB = transaction.objectStore('setlist');
+        if (replace) {
+            let clearRequest = setListFromDB.clear();
+            clearRequest.onerror = (err) => {
+                throw new Error(`Error clearing existing setlist: ${err.message}`);
+            }
+            clearRequest.onsuccess = () => {
+                console.log('replace the whole setlist');
+            }
+        }
         // reset all positions
         let currPos = -1;
         let updateRequest = setListFromDB.openCursor();
         updateRequest.onerror = (err) => {
-            console.log(`Error resetting positions: ${err.target.error.message}`);
+            throw new Error(`Error resetting positions: ${err.target.error.message}`);
         }
         updateRequest.onsuccess = () => {
             let cursor = updateRequest.result;
@@ -156,6 +176,7 @@ const saveSetlist = () => {
                         console.log('Setlist item saved');
                     }
                 }
+                setDownloadButton(setlist);
             }
         }
     }
@@ -181,7 +202,7 @@ const deleteSetlistItem = (e) => {
             }
             transaction.oncomplete = () => {
                 console.log(`${e.target.id} deleted`);
-                slist('setlist');
+                slist(setlistContainer);
             }
             
         }
@@ -206,8 +227,59 @@ const resetApp = () => {
         window.location.href = "index.html";
     }
 }
+
+// create reset button
 const reset = document.createElement('a');
 reset.classList.add('button');
 reset.innerText = 'Reset Application';
 document.getElementById('reset').appendChild(reset);
 reset.addEventListener('click', resetApp);
+
+
+// create save to disk button and filename input
+const fileSave = document.createElement('a');
+fileSave.classList.add('button');
+fileSave.innerText = 'Save Setlist To Disk';
+document.getElementById('files').appendChild(fileSave);
+const saveName = document.createElement('input');
+saveName.type = 'text';
+saveName.placeholder = 'Name for Save File';
+saveName.value = '';
+saveName.addEventListener('change', () => setDownloadButton(setlist));
+document.getElementById('files').appendChild(saveName);
+document.getElementById('files').appendChild(document.createElement('br'));
+const setDownloadButton = (setlist) => {
+    fileSave.download = saveName.value === '' ? 'savedSetlist.json' : `${saveName.value}.json`;
+    fileSave.href = `data:application/octet-stream,${JSON.stringify(setlist)}`;
+}
+
+// https://stackoverflow.com/questions/3582671/how-to-open-a-local-disk-file-with-javascript
+const readFileFromDisk = (file) => {
+    return new Promise((resolve, reject) => {
+        let fr = new FileReader();
+        fr.onload = ()=> resolve(fr.result);
+        fr.readAsText(file)
+    })
+}
+
+const loadSetlistFromDisk = async (e) => {
+    console.log('loadSetlistFromDisk')
+    const loadedSetlistString = await readFileFromDisk(e.target.files[0]);
+    const loadedSetlist = await JSON.parse(loadedSetlistString);
+    setlist = loadedSetlist;
+    createSetlistUI(setlist, setlistContainer);
+    saveSetlist(true);
+}
+// create load file from disk button, to trigger file input element
+// https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications
+const fileButton = document.createElement('a')
+fileButton.classList.add('button');
+fileButton.innerText = 'Load Setlist From Disk';
+document.getElementById('files').appendChild(fileButton);
+fileButton.addEventListener('click', ()=>{
+    if (fileLoad)
+        fileLoad.click()
+}, false);
+const fileLoad = document.createElement('input');
+fileLoad.type = 'file';
+fileLoad.addEventListener('change', loadSetlistFromDisk, false);
